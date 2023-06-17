683838766fbc4f21b6c5e90a3f2b834f8329647e
LJW <king2992@naver.com>
2023-06-17 21:41:27 +0900

    first commit

---
+++ APIServer/src/main/java/com/school/cic/controller/RootController.java
@@ -0,0 +1,15 @@
+package com.school.cic.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/getList")
+    public List<String> getList() {
+        return Arrays.asList("Item1", "Item2", "Item3");
+    }
+}
