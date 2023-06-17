4d185e78d7d66e5477868bbb97882f9da6183d06
LJW <king2992@naver.com>
2023-06-17 21:48:24 +0900

    commit test

--- APIServer/src/main/java/com/school/cic/controller/RootController.java
+++ APIServer/src/main/java/com/school/cic/controller/RootController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RootController {
+public class RootController { 
     @GetMapping("/getList")
     public List<String> getList() {
         return Arrays.asList("Item1", "Item2", "Item3");
